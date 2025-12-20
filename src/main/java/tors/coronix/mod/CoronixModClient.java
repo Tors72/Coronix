e168885847e8647bd25d46b57aee542fc5cbd503
tors1 <dm8990090@gmail.com>
2025-12-20 16:02:17 +0530

    RUBY

--- src/main/java/tors/coronix/mod/CoronixModClient.java
+++ src/main/java/tors/coronix/mod/CoronixModClient.java
@@ -3,6 +3,7 @@ package tors.coronix.mod;
 import net.fabricmc.api.ClientModInitializer;
 
 public class CoronixModClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
 
---
+++ src/main/java/tors/coronix/mod/item/ModItems.java
@@ -0,0 +1,27 @@
+package tors.coronix.mod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import tors.coronix.mod.Coronix;
+import net.minecraft.item.Item;
+
+public class ModItems {
+
+    public final static Item RUBY = registerItem("ruby",new Item(new Item.Settings()));
+    //in lowercase with _ to seperate characters -- used in /give command, convention
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(Coronix.MOD_ID, name), item);
+
+    }
+
+    public static void registerModItems(){
+        Coronix.LOGGER.info("Registering Mod Items for " + Coronix.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+                entries.add(RUBY);
+        });
+    }
+}
