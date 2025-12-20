5f133724ec04065fb854aaffa89d750bafb5f921
tors1 <dm8990090@gmail.com>
2025-12-20 14:18:16 +0530

    Initial commit

---
+++ src/main/java/tors/coronix/mod/CoronixDataGenerator.java
@@ -0,0 +1,11 @@
+package tors.coronix.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CoronixDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/tors/coronix/mod/CoronixModClient.java
@@ -0,0 +1,10 @@
+package tors.coronix.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoronixModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
